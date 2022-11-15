9f9dfa0844175f3fb89b9a72f9e5410b5a94ab95
jaesjeon <jaesjeon@student.42seoul.kr>
2022-11-15 19:49:09 +0900

    ex00 finished

---
+++ 00/ex00/megaphone.cpp
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jaesjeon <jaesjeon@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/15 18:17:03 by jaesjeon          #+#    #+#             */
+/*   Updated: 2022/11/15 19:20:58 by jaesjeon         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <string>
+#include <locale>
+
+#define MSG_NO_ARG "* LOUD AND UNBEARABLE FEEDBACK NOISE *"
+
+int main(int argc, char *argv[])
+{
+    std::locale loc;
+    int idx = 1;
+
+    if (argc > 1)
+    {
+        while (idx < argc)
+        {
+            std::string str(argv[idx++]);
+            for (std::string::size_type i = 0; i < str.length(); ++i)
+                std::cout << std::toupper(str[i], loc);
+        }
+        std::cout << std::endl;
+    }
+    else
+        std::cout << MSG_NO_ARG << std::endl;
+    return 0;
+}
