5c252e5cd8c56e4dfbaf1a85d5e5fe0d897640e0
jaesjeon <jaesjeon@student.42seoul.kr>
2022-11-20 00:14:16 +0900

    CPP03_ex00 finished

---
+++ 03/ex00/ClapTrap.cpp
@@ -0,0 +1,99 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.cpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jaesjeon <jaesjeon@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/19 23:20:30 by jaesjeon          #+#    #+#             */
+/*   Updated: 2022/11/20 00:13:30 by jaesjeon         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include "ClapTrap.hpp"
+#define MSG_CONSTRUCTOR " constructor called"
+#define MSG_DESTRUCTOR "Destructor called"
+#define MSG_OPERATOR " operator called"
+
+const std::string ClapTrap::unitType = "ClapTrap";
+
+ClapTrap::ClapTrap(void): _name("undefined"), _hitPoint(10), _energyPoint(10), _attackDamage(0) {
+    std::cout << "Default" << MSG_CONSTRUCTOR << std::endl;
+    this->printMyStatus();
+}
+
+ClapTrap::~ClapTrap(void) {
+    std::cout << MSG_DESTRUCTOR << std::endl;
+}
+
+ClapTrap::ClapTrap(const ClapTrap &src) {
+    std::cout << "Copy " << MSG_CONSTRUCTOR << std::endl;
+    *this = src;
+    this->printMyStatus();
+}
+
+ClapTrap& ClapTrap::operator= (const ClapTrap &src) {
+    std::cout << "Copy assignment" << MSG_OPERATOR << std::endl;
+    this->_name = src._name;
+    this->_hitPoint = src._hitPoint;
+    this->_energyPoint = src._energyPoint;
+    this->_attackDamage = src._attackDamage;
+    this->printMyStatus();
+    return *this;
+}
+
+ClapTrap::ClapTrap(std::string name): _name(name), _hitPoint(10), _energyPoint(10), _attackDamage(0) {
+    std::cout << this->_name << MSG_CONSTRUCTOR << std::endl;
+    this->printMyStatus();
+}
+
+void ClapTrap::printMyStatus(void) const {
+    std::cout << "Name : " << this->_name;
+    std::cout << " | ";
+    std::cout << "HP : " << this->_hitPoint;
+    std::cout << " | ";
+    std::cout << "EP : " << this->_energyPoint;
+    std::cout << " | ";
+    std::cout << "AD : " << this->_attackDamage;
+    std::cout << std::endl;
+}
+
+bool ClapTrap::checkCanAction(void) const {
+    if (this->_hitPoint == 0) {
+        std::cout << "HP is 0!" << std::endl;
+        return false;
+    } else if (this->_energyPoint == 0) {
+        std::cout << "EP is 0!" << std::endl;
+        return false;
+    }
+    return true;
+}
+
+void ClapTrap::attack(const std::string &target) {
+    if (this->checkCanAction()) {
+        this->_energyPoint -=1;
+        std::cout << ClapTrap::unitType << " " << this->_name << " attacks " << target \
+                    << ", causing " << this->_attackDamage << " points of damage!" << std::endl;
+    }
+}
+
+void ClapTrap::takeDamage(unsigned int amount) {
+    if (this->_hitPoint == 0) {
+        std::cout << "HP is already 0!" << std::endl;
+        return ;
+    }
+    if (this->_hitPoint <= amount) this->_hitPoint = 0;
+    else this->_hitPoint -= amount;
+    std::cout << ClapTrap::unitType << " " << this->_name << " takes " << amount << " points of damage" << std::endl;
+    std::cout << ClapTrap::unitType << " " << this->_name << " HP : " << this->_hitPoint << std::endl;
+}
+
+void ClapTrap::beRepaired(unsigned int amount) {
+    if (this->checkCanAction()) {
+        this->_hitPoint += amount;
+        this->_energyPoint -=1;
+        std::cout << ClapTrap::unitType << " " << this->_name << " is repaired " << amount << " points of HP" << std::endl;
+        std::cout << ClapTrap::unitType << " " << this->_name << " HP : " << this->_hitPoint << std::endl;
+    }
+}
---
+++ 03/ex00/main.cpp
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jaesjeon <jaesjeon@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/19 23:53:50 by jaesjeon          #+#    #+#             */
+/*   Updated: 2022/11/20 00:07:38 by jaesjeon         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include "ClapTrap.hpp"
+
+int main(void) {
+    std::cout << "HP 0 case" << std::endl;
+    
+    ClapTrap userA("42seoul");
+    userA.attack("SSAFY");
+    userA.takeDamage(5);
+    userA.beRepaired(2);
+    userA.attack("UTEKO");
+    userA.takeDamage(5);
+    userA.attack("SSAFY");
+    userA.beRepaired(2);
+    userA.takeDamage(5);
+    userA.attack("SSAFY");
+    userA.beRepaired(2);
+    userA.takeDamage(5);
+
+    std::cout << std::endl;
+    std::cout << "EP 0 case" << std::endl;
+
+    ClapTrap userB("jaesjeon");
+    userB.attack("42seoul_1");
+    userB.attack("42seoul_2");
+    userB.attack("42seoul_3");
+    userB.attack("42seoul_4");
+    userB.attack("42seoul_5");
+    userB.attack("42seoul_6");
+    userB.attack("42seoul_7");
+    userB.attack("42seoul_8");
+    userB.attack("42seoul_9");
+    userB.attack("42seoul_10");
+    userB.attack("42seoul_11");
+    userB.beRepaired(2);
+    userB.takeDamage(5);
+    userB.takeDamage(7);
+    userB.printMyStatus();
+    return 0;
+}
