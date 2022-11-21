5e23d5d85dcad0274f8562821ff5b51c58f09b4f
jaesjeon <jaesjeon@student.42seoul.kr>
2022-11-21 17:52:24 +0900

    CPP03_ex01 main modified

--- 03/ex01/main.cpp
+++ 03/ex01/main.cpp
@@ -6,11 +6,12 @@
 /*   By: jaesjeon <jaesjeon@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/19 23:53:50 by jaesjeon          #+#    #+#             */
-/*   Updated: 2022/11/20 22:41:50 by jaesjeon         ###   ########.fr       */
+/*   Updated: 2022/11/21 17:51:49 by jaesjeon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include <unistd.h>
 #include "ScavTrap.hpp"
 
 int main(void) {
@@ -31,6 +32,18 @@ int main(void) {
         std::cout << "-------------------------------------" << std::endl;
         saffy.attack("ftseoul");
         saffy.guardGate();
+    }
+    std::cout << "#####################################" << std::endl;
+    {
+        std::cout << "-------------------------------------" << std::endl;
+        ClapTrap *upCastedSaffy = new ScavTrap("upCastedSaffy");
+        std::cout << "-------------------------------------" << std::endl;
+        upCastedSaffy->printMyStatus();
+        std::cout << "-------------------------------------" << std::endl;
+        upCastedSaffy->attack("ftseoul");
+        // upCastedSaffy->guardGate();
+        delete upCastedSaffy;
+        system("leaks UnitTrap2");
     }
     return 0;
 }
