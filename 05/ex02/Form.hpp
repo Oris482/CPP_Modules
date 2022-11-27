af5aa451be9db30494cb6abf142cac681745c91c
jaesjeon <jaesjeon@student.42seoul.kr>
2022-11-27 18:29:51 +0900

    CPP05_ex02 referenceTree to string, Form class protected to private

--- 05/ex02/Form.hpp
+++ 05/ex02/Form.hpp
@@ -6,7 +6,7 @@
 /*   By: jaesjeon <jaesjeon@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/26 16:14:53 by jaesjeon          #+#    #+#             */
-/*   Updated: 2022/11/26 22:42:06 by jaesjeon         ###   ########.fr       */
+/*   Updated: 2022/11/27 18:19:35 by jaesjeon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,9 +24,8 @@
 class Bureaucrat;
 
 class Form {
-    protected:
+    private:
         Form(void);
-        explicit Form(const std::string name, const int gradeToSign, const int gradeToExecute);
         
         const std::string _name;
         bool _isSigned;
@@ -35,16 +34,13 @@ class Form {
 
         static const unsigned int maxGrade = 1;
         static const unsigned int minGrade = 150;
-        static const std::out_of_range GradeTooHighException;
-        static const std::out_of_range GradeTooLowException;
-        static const std::runtime_error NotSignedFormException;
-        static const std::runtime_error AlreadySignedFormException;
-        static const std::runtime_error FileOpenException;
 
     public:
         ~Form(void);
         Form(const Form &src);
         Form& operator= (const Form &src);
+        
+        explicit Form(const std::string name, const int gradeToSign, const int gradeToExecute);
 
         const std::string& getName(void) const;
         bool getIsSigned(void) const;
@@ -53,6 +49,12 @@ class Form {
 
         void beSigned(const Bureaucrat &manager);
         virtual void execute(Bureaucrat const &executor) const = 0;
+        
+        static const std::out_of_range GradeTooHighException;
+        static const std::out_of_range GradeTooLowException;
+        static const std::runtime_error NotSignedFormException;
+        static const std::runtime_error AlreadySignedFormException;
+        static const std::runtime_error FileOpenException;
 };
 
 std::ostream& operator<< (std::ostream& os, const Form &target);
