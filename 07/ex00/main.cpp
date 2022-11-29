c758088ecab78b7c07839fb46496f9a65682cfde
jaesjeon <jaesjeon@student.42seoul.kr>
2022-11-29 18:37:18 +0900

    CPP07_ex00 add swap which get reference parameter

--- 07/ex00/main.cpp
+++ 07/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jaesjeon <jaesjeon@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/29 03:19:02 by jaesjeon          #+#    #+#             */
-/*   Updated: 2022/11/29 03:35:24 by jaesjeon         ###   ########.fr       */
+/*   Updated: 2022/11/29 18:36:31 by jaesjeon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,13 +18,15 @@ int main(void) {
     {
         int a = 2;
         int b = 3;
-        ::swap( &a, &b );
+        ::swap( a, b );
+        // ::swap( &a, &b );
         std::cout << "a = " << a << ", b = " << b << std::endl;
         std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
         std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
         std::string c = "chaine1";
         std::string d = "chaine2";
-        ::swap(&c, &d);
+        ::swap(c, d);
+        // ::swap(&c, &d);
         std::cout << "c = " << c << ", d = " << d << std::endl;
         std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
         std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
