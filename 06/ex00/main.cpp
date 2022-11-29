5ead6c5a94c110a8c94649492296d32fbe5cd5a8
jaesjeon <jaesjeon@student.42seoul.kr>
2022-11-29 15:55:35 +0900

    CPP06_ex00 main update

--- 06/ex00/main.cpp
+++ 06/ex00/main.cpp
@@ -6,16 +6,20 @@
 /*   By: jaesjeon <jaesjeon@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/27 17:49:39 by jaesjeon          #+#    #+#             */
-/*   Updated: 2022/11/28 14:25:19 by jaesjeon         ###   ########.fr       */
+/*   Updated: 2022/11/29 15:55:22 by jaesjeon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
 #include "ConvertSampler.hpp"
 
 int main(int argc, char *argv[]) {
     ConvertSampler converter;
 
-    if (argc != 2) return 1;
+    if (argc != 2) {
+        std::cout << "Only 1 argument required" << std::endl;
+        return 1;
+    }
     
     converter.input(argv[1]);
     converter.printSampler();
