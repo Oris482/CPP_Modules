e9268b8e2f5d971cfbf42ba8e323f84ad3a77a0a
jaesjeon <jaesjeon@student.42seoul.kr>
2022-12-04 19:22:14 +0900

    CPP07_ex01 redo finished

--- 07/ex01/iter.hpp
+++ 07/ex01/iter.hpp
@@ -6,13 +6,15 @@
 /*   By: jaesjeon <jaesjeon@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/29 03:39:00 by jaesjeon          #+#    #+#             */
-/*   Updated: 2022/11/29 03:51:15 by jaesjeon         ###   ########.fr       */
+/*   Updated: 2022/12/04 19:21:16 by jaesjeon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 #ifndef ITER_H__
 # define ITER_H__
+
+# include <iostream>
 
 template <typename T>
 void iter(T* array, unsigned int length, void (*f)(T&)) {
@@ -22,8 +24,21 @@ void iter(T* array, unsigned int length, void (*f)(T&)) {
 }
 
 template <typename T>
+void iter(const T* array, unsigned int length, void (*f)(const T&)) {
+    for (unsigned int i = 0; i < length; ++i) {
+        f(array[i]);
+    }
+}
+
+template <typename T>
 void plusOne(T& target) {
     target += 1;
 }
+
+template <typename T>
+void printAll(const T& target) {
+    std::cout << target << std::endl;
+}
+
 
 #endif
--- 07/ex01/main.cpp
+++ 07/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jaesjeon <jaesjeon@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/29 03:47:00 by jaesjeon          #+#    #+#             */
-/*   Updated: 2022/11/29 03:53:31 by jaesjeon         ###   ########.fr       */
+/*   Updated: 2022/12/04 19:20:11 by jaesjeon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,26 +15,18 @@
 
 int main(void) {
     int intArray[5] = {1, 3, 5, 7, 9};
-    for (int i = 0; i < 5; ++i) {
-        std::cout << intArray[i];
-    }
+    iter(intArray, 5, printAll);
     std::cout << std::endl;
     iter(intArray, 5, plusOne);
-    for (int i = 0; i < 5; ++i) {
-        std::cout << intArray[i];
-    }
+    iter(intArray, 5, printAll);
     std::cout << std::endl;
 
     std::cout << "----------------------------------------------" << std::endl;
     char charArray[5] = {'a', 'b', 'c', 'd', 'e'};
-    for (int i = 0; i < 5; ++i) {
-        std::cout << charArray[i];
-    }
+    iter(charArray, 5, printAll);
     std::cout << std::endl;
     iter(charArray, 5, plusOne);
-    for (int i = 0; i < 5; ++i) {
-        std::cout << charArray[i];
-    }
+    iter(charArray, 5, printAll);
     std::cout << std::endl;
 
     return 0;
